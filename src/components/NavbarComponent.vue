a9b960980cbdf1d8fdd72080ef3182c0ae4862a5
caijiW <caiji202111@gmail.com>
2023-03-17 00:21:43 +0800

    first commit

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <router-link class="nav-link" to="/">Home</router-link>
+        <router-link class="nav-link" to="/about">About</router-link>
+        <router-link class="nav-link" to="/products">Products</router-link>
+        <router-link class="nav-link" to="/cart">購物車</router-link>
+        <router-link class="nav-link" to="/login">後台</router-link>
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
